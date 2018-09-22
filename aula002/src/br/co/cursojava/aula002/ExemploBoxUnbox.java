75b1f37370d0a489eb197b5bdc1d3486f1198ae4
Felipe <felipe.darosaoliveira@gmail.com>
2018-09-22 10:49:04 -0300

    aula002 conversores wrapper

---
+++ aula002/src/br/co/cursojava/aula002/ExemploBoxUnbox.java
@@ -0,0 +1,24 @@
+package br.co.cursojava.aula002;
+
+public class ExemploBoxUnbox {
+	
+	public static void main(String[] args) {
+		
+		//pré java 5
+		int numero = 10;
+		Integer wrapper = new Integer(numero);
+		int num = wrapper.intValue();
+		num++;
+		wrapper = new Integer(num);
+		
+		//pós Java 5
+		int numero2 = 10;
+		Integer wrapper2 = numero2;
+		wrapper2++;
+		
+
+		
+		
+	}
+
+}
